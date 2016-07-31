4adcd0a334c38b1a2f033b1363918c72edb23527
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-31 12:36:17 -0400

    Create two pane view of backlog list and workitem details.

--- src/shared/components/Landing.js
+++ src/shared/components/Landing.js
@@ -12,16 +12,59 @@ export class LandingContainer extends React.Component {
 
     render() {
         const wis = this.props.workitems.map(wi => {
-            const children = wi.children.map(child => <div key={child}>---------->{child}</div>);
-            return <div key={wi.number} onClick={this.onClick.bind(this, wi.oid)}>
-                <span>{wi.isSelected ? "Y" : "N"} - {wi.oid} - {wi.number} - {wi.name}</span>
-                <div>{children}</div>
-            </div>
+            return (
+                <tr key={wi.number} id={wi.oid}
+                    className={`${wi.isSelected ? 'selected' : ''}`}
+                    onClick={this.onClick.bind(this, wi.oid)}>
+                    <td></td>
+                    <td className="number">{wi.number}</td>
+                    <td className="name">{wi.name}</td>
+                    <td></td>
+                </tr>
+            )
         });
 
         return (
-            <div>
-                {wis}
+            <div className="backlog-content-container">
+                <div className="backlog-content">
+                    <div className="gutter"></div>
+                    <div className="left backlog">
+                        <div className="pane">
+                            <div className="header">
+                                <div className="toolbar">
+                                    <div className="btn btn-primary">Add</div>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <table className="grid">
+                                    <colgroup>
+                                        <col className="spacer" />
+                                        <col className="number" />
+                                        <col className="name" />
+                                        <col className="select-arrow" />
+                                    </colgroup>
+                                    <tbody>
+                                    {wis}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right workitem-details">
+                        <div className="pane">
+                            <div className="header">
+                                <div className="toolbar">
+                                    <div className="btn btn-primary">Add</div>
+                                </div>
+                                <div className="content">
+                                    Update the workitem here...
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pane-caret"></div>
+                    </div>
+                    <div className="gutter"></div>
+                </div>
             </div>
         )
     }
