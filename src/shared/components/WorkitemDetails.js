306f4c94b74892eb926ef9fbd704150ca12718e6
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-31 18:45:55 -0400

    Pass default props for the workitem details and manage workitem with internal state that is overridden on prop changes.

--- src/shared/components/WorkitemDetails.js
+++ src/shared/components/WorkitemDetails.js
@@ -1,8 +1,70 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { ActionCreators as WorkitemActions } from './../atoms/workitem';
 
 export class WorkitemDetailsContainer extends React.Component {
+
+    static defaultProps = {
+        workitem: {
+            oid: '',
+            assetType: '',
+            number: '',
+            name: '',
+            scope: '',
+            changeDate: '',
+            classOfService: '',
+            estimate: '',
+            status: '',
+            blockingIssues: [],
+            owners: [],
+            children: []
+        }
+    };
+
+
+    constructor(props, context) {
+        super(props, context);
+        const wi = props.workitem;
+        this.state = {
+            oid: wi.oid,
+            assetType: wi.assetType,
+            number: wi.number,
+            name: wi.name,
+            scope: wi.scope,
+            changeDate: wi.changeDate,
+            classOfService: wi.classOfService,
+            estimate: wi.estimate,
+            status: wi.status,
+            blockingIssues: wi.blockingIssues,
+            owners: wi.owners,
+            children: wi.children
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const wi = nextProps.workitem;
+        this.setState({
+            oid: wi.oid,
+            assetType: wi.assetType,
+            number: wi.number,
+            name: wi.name,
+            scope: wi.scope,
+            changeDate: wi.changeDate,
+            classOfService: wi.classOfService,
+            estimate: wi.estimate,
+            status: wi.status,
+            blockingIssues: wi.blockingIssues,
+            owners: wi.owners,
+            children: wi.children
+        });
+    }
+
+    updateName(event) {
+        const newName = event.target.value;
+        this.setState({ name: newName });
+        //this.props.updateWorkitem('name', newName);
+    }
 
     render() {
         const {
@@ -17,17 +79,27 @@ export class WorkitemDetailsContainer extends React.Component {
             status,
             blockingIssues,
             owners,
-            children,
-            isSelected,
-            isHovered,
-        } = this.props.workitem;
+            children
+        } = this.state;
+
+        const blockingIssuesList = blockingIssues.map(blockingIssue => <div key={blockingIssue}>{blockingIssue}</div>);
+        const ownersList = owners.map(owner => <div key={owner}>{owner}</div>);
+        const testsAndTasksList = children.map(child => <div key={child}>{child}</div>);
 
         return (
             <div>
                 <div>{oid}</div>
+                <input type="text" onChange={this.updateName.bind(this)} value={this.state.name} />
                 <div>{name}</div>
                 <div>{number}</div>
                 <div>{changeDate}</div>
+                <div>{scope}</div>
+                <div>{classOfService}</div>
+                <div>{estimate}</div>
+                <div>{status}</div>
+                <div>{blockingIssuesList}</div>
+                <div>{ownersList}</div>
+                <div>{testsAndTasksList}</div>
             </div>
         )
     }
@@ -35,7 +107,7 @@ export class WorkitemDetailsContainer extends React.Component {
 
 function mapStateToProps(state) {
     return {
-        workitem: state.workitemStateAtom.get('workitems').get(state.workitemStateAtom.get('selected')) || {}
+        workitem: state.workitemStateAtom.get('workitems').get(state.workitemStateAtom.get('selected'))
     }
 }
 
