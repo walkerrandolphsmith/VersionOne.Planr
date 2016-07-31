dfcbc98c3f2b85a8597f1b8342fcae6daaa19b14
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-31 19:28:45 -0400

    getWorkitemDetails updates the right node in the workitemStateAtom.

--- src/shared/atoms/workitem/Reducer/updateWorkitemWithDetails.js
+++ src/shared/atoms/workitem/Reducer/updateWorkitemWithDetails.js
@@ -1,7 +1,8 @@
 import * as actions from './../Actions';
 
 const updateWorkitemWithDetails = {
-    [actions.updateWorkitemWithDetails]: (state, action) => state.set(action.payload.workitemWithDetails.oid, action.payload.workitemWithDetails)
+    [actions.updateWorkitemWithDetails]: (state, action) => state
+        .setIn(["workitems", action.payload.workitemWithDetails.oid], action.payload.workitemWithDetails)
 };
 
 export default updateWorkitemWithDetails;
