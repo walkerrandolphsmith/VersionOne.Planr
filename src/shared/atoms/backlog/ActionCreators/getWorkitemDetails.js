d574256edf54e52007047357e05e40227958ab5f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-01 21:22:07 -0400

    Update the workitem details with its relations

--- src/shared/atoms/backlog/ActionCreators/getWorkitemDetails.js
+++ src/shared/atoms/backlog/ActionCreators/getWorkitemDetails.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { ActionCreators as WorkitemActions } from './../../workitem';
 import { Records as WorkitemRecords } from './../../workitem';
+
 import { setCaretPosition } from './index';
 
 export default (i, workitemOidToken) => (dispatch, getState) => {
@@ -9,36 +10,39 @@ export default (i, workitemOidToken) => (dispatch, getState) => {
     axios
         .get(`/api/workitem/${workitemOidToken.replace(':', '-')}`)
         .then(response => {
-            const workitems = response.data;
+            const workitem = response.data;
 
-            const owners = workitems.Owners.map((owner, i) => {
+            const owners = workitem.Owners.map((owner, i) => {
                 return {
                     oid: owner._oid,
-                    name: workitems['Owners.Name'][i],
-                    avatar: workitems['Owners.Avatar.Content'][i]
+                    name: workitem['Owners.Name'][i],
+                    avatar: workitem['Owners.Avatar.Content'][i]
                 };
             });
 
-            const blockingIssues = workitems.BlockingIssues.map((blockingIssue, i) => {
+            const blockingIssues = workitem.BlockingIssues.map((blockingIssue, i) => {
                 return {
                     oid: blockingIssue._oid,
-                    name: workitems['BockingIssues.Name'][i]
+                    name: workitem['BockingIssues.Name'][i]
                 }
             });
 
             const classOfService = {
-                oid: workitems.ClassOfService._oid,
-                name: workitems['ClassOfService.Name']
+                oid: workitem.ClassOfService._oid,
+                name: workitem['ClassOfService.Name']
             };
 
             const scope = {
-                oid: workitems.Scope._oid,
-                name: workitems['Scope.Name']
+                oid: workitem.Scope._oid,
+                name: workitem['Scope.Name']
             };
 
-            debugger;
+            let workitemWithDetails = WorkitemRecords.createWorkitemRecord(workitem);
+            workitemWithDetails = workitemWithDetails.set('owners', owners);
+            workitemWithDetails = workitemWithDetails.set('scope', scope);
+            workitemWithDetails = workitemWithDetails.set('classOfService', classOfService);
+            workitemWithDetails = workitemWithDetails.set('blockingIssues', blockingIssues);
 
-            const workitemWithDetails = WorkitemRecords.createWorkitemRecord(workitems);
             dispatch(WorkitemActions.updateWorkitemWithDetails(workitemWithDetails));
         })
         .catch(error => {
