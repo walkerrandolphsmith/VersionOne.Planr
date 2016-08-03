b0418f2427465f76435fa00323449b724864d0e6
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-02 20:57:11 -0400

    Simply routes. They are already declarative...

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -5,27 +5,11 @@ import { App } from './components/app';
 import { NotFound } from './components/NotFound';
 import { Landing } from './components/Landing';
 
-export const ROUTES = [
-      {
-            path: '*',
-            component: NotFound,
-            status: 404
-      }
-];
-
-export const getRoutesForRouteDefinitions = routeDefinitions => routeDefinitions.map(
-    (route, i) => <Route key={i} path={route.path} component={route.component} status={route.status} />
+const routes = (
+    <Route path="/" component={App}>
+        <IndexRoute component={Landing} />
+        <Route path={'*'} component={NotFound} status={404} />
+    </Route>
 );
 
-export const RouterFactory = routeDefinitions => {
-      const routes = getRoutesForRouteDefinitions(routeDefinitions);
-
-      return (
-          <Route path="/" component={App}>
-                <IndexRoute component={Landing} />
-                {routes}
-          </Route>
-      );
-};
-
-export default RouterFactory(ROUTES);
+export default routes;
