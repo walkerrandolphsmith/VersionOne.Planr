6ef3b7cc1692cbdac17bf31e8ae63d5e685f490d
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-04 15:51:16 -0400

    Upgrade v1sdk and query activity stream api.

--- src/server/routes/api/activityStream.js
+++ src/server/routes/api/activityStream.js
@@ -2,19 +2,9 @@ import getV1 from './V1Server';
 
 export default oid => {
     const v1 = getV1();
-    return new Promise((resolve, reject) => {
-        resolve({
-            data: {
-
-            }
-        })
-    });
-    /*
-    Waiting on the V1SDK to update and support activity stream.
-    v1
+    return v1
         .getActivityStream(oid)
         .then(response => {
             return response.data;
         });
-    */
 }
