c3d590a8aceeb2d344749e3b9261bd6e4aef683e
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-04 10:17:35 -0400

    Update activity stream endpoint to not throw 500. Commnet expected interface on next sdk update.

--- src/server/routes/api/activityStream.js
+++ src/server/routes/api/activityStream.js
@@ -2,10 +2,19 @@ import getV1 from './V1Server';
 
 export default oid => {
     const v1 = getV1();
-    return new Promise();
-    /*return v1
+    return new Promise((resolve, reject) => {
+        resolve({
+            data: {
+
+            }
+        })
+    });
+    /*
+    Waiting on the V1SDK to update and support activity stream.
+    v1
         .getActivityStream(oid)
         .then(response => {
             return response.data;
-        });*/
+        });
+    */
 }
