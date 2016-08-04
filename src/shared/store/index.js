a66c1f7ebe92568d3df7ff18801891f95b0e3a34
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2016-08-04 13:08:39 -0400

    Implement redux devtools on both client and server

--- src/shared/store/index.js
+++ src/shared/store/index.js
@@ -1,10 +1,21 @@
-import { createStore, compose } from 'redux';
-import middlewareBuilder from './middlewareBuilder';
+import { createStore, compose} from 'redux';
+import createMiddlewares from './createMiddlewares';
 import rootReducer from './rootReducer';
+import devTools from 'remote-redux-devtools';
+import env from './../../shared/env';
 
 export default ({ initialState, history }) => {
-    const createStoreWithMiddleware = compose(...middlewareBuilder(history))(createStore);
-    const store = createStoreWithMiddleware(rootReducer, initialState);
+ if (instanceName === 'Client'){
+     debugger;
+ }
+    let enhancers = [createMiddlewares(history)];
+    if(env.nodeEnv !== 'production') {
+        enhancers = enhancers.concat([
+            typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f,
+            devTools()
+        ]);
+    }
+    const store = createStore(rootReducer, initialState, compose(...enhancers));
 
     if(module.hot) {
         module.hot.accept('./rootReducer', () => {
