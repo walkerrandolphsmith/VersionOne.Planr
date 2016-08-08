92098c7091e2c4a3690572e4cf291e4f50ceb744
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-08 13:26:28 -0400

    Import env as default and then deconstruct it.

--- src/shared/lib/V1Server.js
+++ src/shared/lib/V1Server.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import sdk, {axiosConnector} from 'v1sdk';
-import {v1Host, v1Instance, v1User, v1Password} from './../env';
+import env from './../env';
+
+const { v1Host, v1Instance, v1User, v1Password } = env;
 
 const axiosConnectedSdk = axiosConnector(axios)(sdk);
 export default axiosConnectedSdk(v1Host, v1Instance)
