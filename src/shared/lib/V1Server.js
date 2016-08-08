ace66a65036a2dadfcdc2772c6c3ffe6d1389afa
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-08 13:41:24 -0400

    Remove whitespace.

--- src/shared/lib/V1Server.js
+++ src/shared/lib/V1Server.js
@@ -3,7 +3,6 @@ import sdk, {axiosConnector} from 'v1sdk';
 import env from './../env';
 
 const { v1Host, v1Instance, v1User, v1Password } = env;
-
 const axiosConnectedSdk = axiosConnector(axios)(sdk);
 export default axiosConnectedSdk(v1Host, v1Instance)
     .withCreds(v1User, v1Password);
