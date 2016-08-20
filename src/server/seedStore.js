c2a1328b5a323e42a3ccff90d8c974f8aae81777
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-20 18:08:31 -0400

    Request PrimaryWorkitems

--- src/server/seedStore.js
+++ src/server/seedStore.js
@@ -1,18 +1,14 @@
 import { Records } from './../shared/atoms/workitem';
 import v1 from './../shared/lib/V1Server';
 
-const getBacklog = () => {
-    return v1.query({
-        from: 'Workitem',
+export default (url) => v1.query({
+        from: 'PrimaryWorkitem',
         select: ['Name', 'Number', 'Children'],
         page: {
             start: 0,
             size: 50
         }
     })
-};
-
-export default (url) => getBacklog()
     .then(response => {
         const workitems = response.data[0].reduce((workitems, workitem) => {
             workitems[workitem._oid] = Records.createWorkitemRecord(workitem);
