b672ab6b12628e1d8b176ee0c19cf58b58dea1de
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-24 22:37:58 -0400

    server augmentation occures directly in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,50 @@
+import path from 'path';
 import express from 'express';
-import configureServer from './app';
+import expressPromise from 'express-promise';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import defaultRoute from './defaultRoute';
+import apiRouter from './apiRouter';
 import env from './../shared/env';
+var config = require('./../../webpack.config');
 
-const { host, port } = env;
+const { nodeEnv, host, port, devHost, devPort } = env;
 
-let app = express();
-configureServer(app);
-
-export default app;
-
-app.listen(port, error => {
-    if(error) {
-        console.error(error);
+const logger = (err) => {
+    if(err) {
+        console.error(err);
     } else {
         console.info(`==> 🌎  Listening on port ${port}. Open up http://${host}:${port}/ in your browser.`);
     }
-});
+};
+
+const app = express();
+
+app.use(expressPromise());
+app.use(cors());
+app.use(bodyparser.json());
+
+if(nodeEnv === 'development') {
+    app.use(express.static(path.join(__dirname, './../../../src')));
+
+    const compiler = webpack(config);
+    const devServerSettings = {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    };
+    app.use(webpackDevMiddleware(compiler, devServerSettings));
+    app.use(webpackHotMiddleware(compiler));
+
+    new WebpackDevServer(webpack(config), config.devServer).listen(devPort, devHost, logger);
+}
+
+app.use('/api', apiRouter);
+app.use('/', defaultRoute);
+
+export default app;
+
+app.listen(port, logger);
--- src/shared/env.js
+++ src/shared/env.js
@@ -1,6 +1,7 @@
 export default {
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 3000,
+    devHost: process.env.DEV_HOST || 'localhost',
     devPort: process.env.DEV_PORT || 3001,
     apiHost: process.env.API_HOST || 'localhost',
     apiPort: process.env.API_PORT || 3002,
