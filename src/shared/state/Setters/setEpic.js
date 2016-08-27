4603c18e97758f53f44a158800035cad5682e15c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-26 21:19:37 -0400

    Setting epic also queries for children workitem to filter the backlog.

--- src/shared/state/Setters/setEpic.js
+++ src/shared/state/Setters/setEpic.js
@@ -1,19 +1,62 @@
 import { createAction } from 'redux-actions';
 import axios from 'axios';
 
-const ACTION = 'SET_EPIC';
+const SET_EPIC_ACTION = 'SET_EPIC_ACTION';
+const SET_WORKITEMS_ACTION = 'SET_WORKITEMS_ACTION';
 
-export const setEpic = createAction(
-    ACTION,
-    (epicOidToken) => ({epicOidToken})
+const selectEpic = createAction(
+    SET_EPIC_ACTION,
+    (epicOidToken) => ({ epicOidToken })
 );
+
+const success = createAction(
+    SET_WORKITEMS_ACTION,
+    (workitems) => ({ workitems })
+);
+
+export const setEpic = (epicOidToken) => (dispatch, getState) => {
+    dispatch(selectEpic(epicOidToken));
+    axios
+        .post('/api/query', {
+            'from': 'PrimaryWorkitem',
+            'select': [
+                'Name',
+                'Number'
+            ],
+            'where': {
+                'SuperMeAndUp': epicOidToken
+            }
+        })
+        .then((response) => {
+            const workitems = response.data[0];
+            dispatch(success(workitems));
+        })
+        .catch(err => {
+            console.log('failure')
+        });
+};
 
 const reducer = (state, payload) => {
     state.epic = payload.epicOidToken;
     return { ...state };
 };
 
+const setEpicReducer = (state, payload) => {
+    state.selected = payload.workitems[0]._oid;
+    state.workitems = payload.workitems.reduce((map, workitem) => {
+        map[workitem._oid] = {
+            oid: workitem._oid,
+            assetType: workitem._oid.split(':'),
+            number: workitem.Number,
+            name: workitem.Name
+        };
+        return map;
+    } ,{});
+    return { ...state };
+};
+
 export default {
-    [ACTION]: reducer
+    [SET_EPIC_ACTION]: reducer,
+    [SET_WORKITEMS_ACTION]: setEpicReducer
 };
 
