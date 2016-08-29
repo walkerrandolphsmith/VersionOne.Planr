b05045b39c67fe2063ce2541d0f6996eb02c8549
Alejandro Guzman <alejandro.guzman@versionone.com>
2016-08-29 13:43:28 -0400

    styling on TestList

--- src/shared/components/WorkitemDetails/TestList.js
+++ src/shared/components/WorkitemDetails/TestList.js
@@ -3,20 +3,31 @@ import React from 'react';
 export class TestList extends React.Component {
 
     render() {
-        const {
-            tests
-            } = this.props;
-
-        const testMarkup = tests.map(test => (
-                <div key={test.oid}>
-                    <div>{test.number} - {test.name}</div>
-                </div>
-            ));
+        let i = 0;
+        const tests = this.props.tests.map(test => {
+            return (
+                <tr key={test.number} id={test.oid}
+                    className={`${test.isSelected ? 'selected' : ''} ${test.isHovered ? 'hovered' : ''}`}
+                  >
+                    <td className="number">{test.number}</td>
+                    <td className="name">{test.name}</td>
+                    <td className="edit-icon">i</td>
+                </tr>
+            )
+        });
 
         return (
-            <div>
-                <h2>Test</h2>
-                <div>{testMarkup}</div>
+            <div className="tests-table">
+                <table className="grid">
+                    <colgroup>
+                        <col className="number" />
+                        <col className="name" />
+                        <col className="edit-icon" />
+                    </colgroup>
+                    <tbody>
+                    {tests}
+                    </tbody>
+                </table>
             </div>
         )
     }
