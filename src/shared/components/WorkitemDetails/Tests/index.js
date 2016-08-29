efaf887e4a9a20ec9fb66e7be3f9db7398503110
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-08-29 18:49:51 -0400

    Move TestTemplate to the index.

--- src/shared/components/WorkitemDetails/Tests/index.js
+++ src/shared/components/WorkitemDetails/Tests/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Actionbar } from './ActionBar';
 import { TestList } from './TestList';
-import { TestTemplate } from './TestTemplate';
+import { BddTemplate } from './BddTemplate';
+import { SimpleTemplate } from './SimpleTemplate';
 
 export class TestSection extends React.Component {
 
+    getViewForTab() {
+        const TempalteTypeMap = {
+            0: <BddTemplate {...this.props} />,
+            1: <SimpleTemplate {...this.props} />
+        };
+        return TempalteTypeMap[this.props['currentTestsTab']] || <SimpleTemplate {...this.props} />;
+    }
+
     render() {
         const { tests } = this.props;
+        const template = this.getViewForTab();
         return (
             <div className="test-section">
                 <Actionbar {...this.props} />
-                <TestTemplate {...this.props} />
+                {template}
                 <TestList tests={tests} />
             </div>
         )
