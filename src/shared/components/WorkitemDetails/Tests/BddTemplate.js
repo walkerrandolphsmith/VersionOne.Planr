1a387229f010f3aa6829c9cc3969d60d518cb10d
Keith Gargano <Keith.Gargano@gmail.com>
2016-08-30 10:09:55 -0400

    styling of inputs in bdd form

--- src/shared/components/WorkitemDetails/Tests/BddTemplate.js
+++ src/shared/components/WorkitemDetails/Tests/BddTemplate.js
@@ -37,13 +37,19 @@ export class BddTemplate extends React.Component {
 
     render() {
         return (
-            <div>
-                <label htmlFor="given">Given:</label>
-                <input id="given" name="given" type="text" onChange={this.onChange.bind(this, 'given')} />
-                <label htmlFor="when">When:</label>
-                <input id="when" name="when" type="text" onChange={this.onChange.bind(this, 'when')} />
-                <label htmlFor="then">Then:</label>
-                <input id="then" name="then" type="text" onKeyDown={this.onKeyDown.bind(this)} onChange={this.onChange.bind(this, 'then')} />
+            <div className="bdd-form">
+                <p>
+                    <label htmlFor="given">Given:</label>
+                    <input id="given" name="given" type="text" onChange={this.onChange.bind(this, 'given')} />
+                </p>
+                <p>
+                    <label htmlFor="when">When:</label>
+                    <input id="when" name="when" type="text" onChange={this.onChange.bind(this, 'when')} />
+                </p>
+                <p>
+                    <label htmlFor="then">Then:</label>
+                    <input id="then" name="then" type="text" onKeyDown={this.onKeyDown.bind(this)} onChange={this.onChange.bind(this, 'then')} />
+                </p>
                 <Button text="Add" onClick={this.onClick.bind(this)} />
             </div>
         )
