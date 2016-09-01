3c82afaa31a90083189a4b8013a70563e3805b64
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-01 08:33:01 -0400

    Move inline button styles to less. Ensure the onClick fires when svg is clicked (pointer-events none) Move the click-outside logic to the multibutton!

--- src/shared/components/Buttons/MultiButton.js
+++ src/shared/components/Buttons/MultiButton.js
@@ -1,17 +1,18 @@
 import React, {Component, PropTypes} from 'react';
-import {
-    unstable_renderSubtreeIntoContainer as renderSubtreeIntoContainer,
-    unmountComponentAtNode
-} from 'react-dom';
 import { ChevronIcon } from './../Icons';
 
-const magicWidthOfContainer = 150;
+function ancestorHasClass(element, classname) {
+    if(!element.className || typeof element.className !== 'string')
+        return false;
+    else
+        return element.className.split(' ').indexOf(classname) >= 0
+            ? true
+            : element.parentNode && ancestorHasClass(element.parentNode, classname);
+}
 
 export default class MultiButton extends Component {
     static propTypes = {
         children: PropTypes.node,
-        classNames: PropTypes.string,
-        styles: PropTypes.object,
         text: PropTypes.string.isRequired,
         onClick: PropTypes.func,
         isDisabled: PropTypes.bool
@@ -19,57 +20,38 @@ export default class MultiButton extends Component {
 
     static defaultProps = {
         children: [],
-        classNames: '',
-        styles: {},
-        onClick: () => { console.log("I was not override"); },
+        onClick: () => { },
         isDisabled: false
     };
 
     constructor(props, context) {
         super(props, context);
         this.state = {
-            mountNode: null,
             isShown: false
         }
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ isShown: nextProps.isOpen });
-    }
-
-    componentWillMount() {
-        const mountNode = document.createElement('div');
-        mountNode.className  = 'MultiButtonOptions';
-        document.body.appendChild(mountNode);
-        this.setState({ mountNode: mountNode });
-    }
-
-    componentWillUnmount() {
-        if (this.state.mountNode) {
-            unmountComponentAtNode(this.state.mountNode);
-            this.state.mountNode.parentElement.removeChild(this.state.mountNode);
-            this.setState({ mountNode: null });
+    clickOutsideButtonHandler(event) {
+        const elm = event.target;
+        const isMultiButtonAncestor = ancestorHasClass(elm, 'multi-button');
+        if(!isMultiButtonAncestor) {
+            event.stopPropagation();
+            this.setState({ isShown: false });
         }
     }
 
-    componentWillUpdate(nextProps, nextState) {
-        const props = Object.assign({}, nextProps, nextState);
-        renderSubtreeIntoContainer(
-            this,
-            (<MultiButtonPortal {...props}>{this.props.children}</MultiButtonPortal>),
-            this.state.mountNode
-        );
+    componentDidMount() {
+        window.addEventListener('click', this.clickOutsideButtonHandler.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('click', this.clickOutsideButtonHandler.bind(this));
     }
 
     openOptions(event) {
-        const anchor = event.currentTarget;
-        const position = anchor.getBoundingClientRect();
-
         if (!this.props.isDisabled) {
             this.setState({
-                isShown: !this.state.isShown,
-                top: position.bottom,
-                left: position.right - magicWidthOfContainer
+                isShown: !this.state.isShown
             });
         }
     }
@@ -83,60 +65,21 @@ export default class MultiButton extends Component {
     render() {
         const {
             text,
+            children,
             isDisabled
         } = this.props;
+        
+        const { isShown } = this.state;
 
         return (
-            <div className={`multi-button button ${isDisabled ? 'disabled' : ''}`} ref="multi-button">
+            <div className={`multi-button button ${isDisabled ? 'disabled' : ''}`}>
                 <span className="quick-action" onClick={this.onClick.bind(this)}>{text}</span>
                 <span className="all-actions" onClick={this.openOptions.bind(this)}>
                     <ChevronIcon />
+                     <div className={`multi-button-options ${isShown ? 'expanded' : ''}`}>
+                        {children}
+                    </div>
                 </span>
-            </div>
-        );
-    }
-}
-
-class MultiButtonPortal extends Component {
-    static propTypes = {
-        children: PropTypes.node,
-        isShown: PropTypes.bool,
-        top: PropTypes.number,
-        left: PropTypes.number,
-        classNames: PropTypes.string,
-        styles: PropTypes.object
-    };
-
-    static defaultProps = {
-        children: [],
-        isShown: false,
-        top: 0,
-        left: 0,
-        classNames: '',
-        styles: {}
-    };
-
-    render() {
-        const { children, isShown, top, left, classNames, styles } = this.props;
-        const finalStyles = Object.assign(
-            {
-                zIndex: '1000',
-                width: `${magicWidthOfContainer}px`,
-                cursor: 'pointer'
-            },
-            styles,
-            {
-                position: 'absolute',
-                overflow: 'hidden',
-                top: top,
-                left: left,
-                display: isShown ? 'block' : 'none'
-            }
-        );
-
-        return (
-            <div className={`multi-button-options ${classNames}`} style={finalStyles} >
-                {children}
             </div>
         );
     }
