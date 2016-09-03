ab048286a2e96417c465920f1cb4b49c8c232818
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-03 13:17:27 -0400

    Create an api server on a different domain in order to enable dispatching actions on the server to seed data. This enables presitent routes!! :)

---
+++ src/apiServer/index.js
@@ -0,0 +1,121 @@
+import express from 'express';
+import expressPromise from 'express-promise';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import axios from 'axios';
+import v1 from './V1Server';
+import { apiHost, apiPort, v1Protocol, v1Host, v1Instance } from './../shared/env';
+
+const app = express();
+
+app.use(expressPromise());
+app.use(cors());
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+app.get('/validate', (req, res) => {
+    console.log('found');
+    const authToken = req.header('Authorization') || req.cookies.Authorization;
+    const rawToken = typeof authToken == 'string' ? authToken.split(' ')[1] : "";
+
+    v1(authToken)
+        .query({ 
+            from: 'Grant',
+            select: [
+                'Owner.Avatar.Content'
+            ],
+            where: {
+                Token: rawToken
+            }
+        })
+        .then(response => {
+            res
+                .cookie('Authorization', authToken, { maxAge: 900000, httpOnly: false })
+                .status(200)
+                .send(response.data);
+        }).catch((err) => {
+            //console.log('err', err);
+            res.clearCookie('Authorization').status(401).send();
+        });
+});
+
+app.get('/activitystream/:id', (req, res) => {
+    const oid = req.originalUrl.split('/activitystream/')[1];
+    const authToken = req.cookies.Authorization;
+    v1(authToken).getActivityStream(oid).then(response => {
+        res.status(200).send(response.data);
+    });
+});
+
+app.get('/conversationstream/:id', (req, res) => {
+    const oid = req.originalUrl.split('/conversationstream/')[1];
+    const authToken = req.cookies.Authorization;
+    const url = `${v1Protocol}://${v1Host}/${v1Instance}/Mobile.mvc/GetConversationStream?involving=${oid}`;
+    axios.get(url, {
+        headers: {
+            'Authorization': authToken
+        }
+    }).then(response => {
+        res.status(200).send(response.data);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.get('/conversationthread/:id', (req, res) => {
+    const oid = req.originalUrl.split('/conversationthread/')[1];
+    const authToken = req.cookies.Authorization;
+    const url = `${v1Protocol}://${v1Host}/${v1Instance}/Mobile.mvc/GetConversationThread?Oid=${oid}`;
+    axios.get(url, {
+        headers: {
+            'Authorization': authToken
+        }
+    }).then(response => {
+        res.status(200).send(response.data);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+app.use('/query', (req, res) => {
+    console.log('query', req.cookies.Authorization);
+    const authToken = req.cookies.Authorization;
+    v1(authToken).query(req.body).then(response => {
+        res.status(200).send(response.data);
+    });
+});
+
+app.post('/create', (req, res) => {
+    const { assetType, assetData } = req.body;
+    const authToken = req.cookies.Authorization;
+    v1(authToken).create(assetType, assetData).then(response => {
+        res.status(200).send(response.data);
+    });
+});
+
+app.post('/update', (req, res) => {
+    const { oidToken, assetData } = req.body;
+    const authToken = req.cookies.Authorization;
+    v1(authToken).update(oidToken, assetData).then(response => {
+        res.status(200).send(response.data);
+    });
+});
+
+app.post('/executeOperation', (req, res) => {
+    const { oidToken, operationName } = req.body;
+    const authToken = req.cookies.Authorization;
+    v1(authToken).executeOperation(oidToken, operationName).then(response => {
+        res.status(200).send(response.data);
+    });
+});
+
+export default app;
+
+app.listen(apiPort, (err) => {
+    if(err) {
+        console.error(err);
+    } else {
+        console.info(`==> 🌎  Listening on port ${apiPort}. Open up http://${apiHost}:${apiPort}/ in your browser.`);
+    }
+});
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+import apiServer from './../apiServer';
 import path from 'path';
 import express from 'express';
 import expressPromise from 'express-promise';
@@ -8,10 +9,9 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import axios from 'axios';
+import httpProxy from 'http-proxy';
 import defaultRoute from './defaultRoute';
-import v1 from './V1Server';
-import { nodeEnv, host, port, devHost, devPort, v1Protocol, v1Host, v1Instance } from './../shared/env';
+import { nodeEnv, host, port, devHost, devPort, apiHost, apiPort } from './../shared/env';
 var config = require('./../../webpack.config');
 
 const logger = (err) => {
@@ -26,8 +26,21 @@ const app = express();
 
 app.use(expressPromise());
 app.use(cors());
-app.use(bodyparser.json());
 app.use(cookieParser());
+
+const apiServerDomain = `http://${apiHost}:${apiPort}`;
+const proxy = httpProxy.createProxyServer({
+    target: apiServerDomain
+});
+
+app.use('/api', (req, res) => {
+    console.log('api route proxing to api server');
+    console.log('original', req.originalUrl);
+    console.log('auth cookie', req.cookies.Authorization);
+    proxy.web(req, res, { target: apiServerDomain });
+});
+//Use body parser middleware after proxying to the api server to prevent the need to buffer the http post methods
+app.use(bodyparser.json());
 
 if(nodeEnv === 'development') {
     app.use(express.static(path.join(__dirname, './../../../src')));
@@ -42,86 +55,6 @@ if(nodeEnv === 'development') {
 
     new WebpackDevServer(webpack(config), config.devServer).listen(devPort, devHost, logger);
 }
-
-app.get('/api/validate', (req, res) => {
-    const authToken = req.header('Authorization') || req.cookies.Authorization;
-    const rawToken = typeof authToken == 'string' ? authToken.split(' ')[1] : "";
-
-    v1(authToken).query({'from': 'Grant', 'select':['Owner.Avatar.Content'], 'where':{'Token':rawToken}}).then(response => {
-        res.cookie('Authorization', authToken, { maxAge: 900000, httpOnly: false }).status(200).send(response.data);
-    }).catch((err) => {
-        console.log('err', err);
-        res.clearCookie('Authorization').status(401).send();
-    });
-});
-
-app.get('/api/activitystream/:id', (req, res) => {
-    const oid = req.originalUrl.split('/activitystream/')[1];
-    const authToken = req.cookies.Authorization;
-    v1(authToken).getActivityStream(oid).then(response => {
-        res.status(200).send(response.data);
-    });
-});
-
-app.get('/api/conversationstream/:id', (req, res) => {
-    const oid = req.originalUrl.split('/conversationstream/')[1];
-    const authToken = req.cookies.Authorization;
-    axios.get(`${v1Protocol}://${v1Host}/${v1Instance}/Mobile.mvc/GetConversationStream?involving=${oid}`, {
-        headers: {
-            'Authorization': authToken
-        }
-    }).then(response => {
-        res.status(200).send(response.data);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
-});
-
-app.get('/api/conversationthread/:id', (req, res) => {
-    const oid = req.originalUrl.split('/conversationthread/')[1];
-    const authToken = req.cookies.Authorization;
-    const url = `${v1Protocol}://${v1Host}/${v1Instance}/Mobile.mvc/GetConversationThread?Oid=${oid}`;
-    axios.get(url, {
-        headers: {
-            'Authorization': authToken
-        }
-    }).then(response => {
-        res.status(200).send(response.data);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
-});
-
-app.post('/api/query', (req, res) => {
-    const authToken = req.cookies.Authorization;
-    v1(authToken).query(req.body).then(response => {
-        res.status(200).send(response.data);
-    });
-});
-
-app.post('/api/create', (req, res) => {
-    const { assetType, assetData } = req.body;
-    const authToken = req.cookies.Authorization;
-    v1(authToken).create(assetType, assetData).then(response => {
-        res.status(200).send(response.data);
-    });
-});
-
-app.post('/api/update', (req, res) => {
-    const { oidToken, assetData } = req.body;
-    const authToken = req.cookies.Authorization;
-    v1(authToken).update(oidToken, assetData).then(response => {
-        res.status(200).send(response.data);
-    });
-});
-
-app.post('/api/executeOperation', (req, res) => {
-    const { oidToken, operationName } = req.body;
-    const authToken = req.cookies.Authorization;
-    v1(authToken).executeOperation(oidToken, operationName).then(response => {
-        res.status(200).send(response.data);
-    });
-});
 
 app.use('/', defaultRoute);
 
