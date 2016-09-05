f2f30e7229d1699d2a15618fab6a9abd7a404707
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-05 00:23:04 -0400

    Clean up env file.

--- src/shared/env.js
+++ src/shared/env.js
@@ -1,13 +1,16 @@
-export const host = process.env.HOST || 'localhost';
-export const port = process.env.PORT || 3000;
-export const devHost = process.env.DEV_HOST || 'localhost';
-export const devPort = process.env.DEV_PORT || 3001;
-export const apiHost = process.env.API_HOST || 'localhost';
-export const apiPort = process.env.API_PORT || 3002;
+const DEFAULT_HOST = 'localhost';
+const {
+    NODE_ENV, HOST, PORT, DEV_HOST, DEV_PORT, API_HOST, API_PORT, V1Protocol, V1Host, V1Instance
+} = process.env;
+
 export const isBrowser = process.browser;
-export const nodeEnv = process.env.NODE_ENV;
-export const v1Protocol = process.env.V1Protocol;
-export const v1Host = process.env.V1Host;
-export const v1Instance = process.env.V1Instance;
-export const v1User = process.env.V1User;
-export const v1Password = process.env.V1Password;
+export const nodeEnv = NODE_ENV;
+export const host = HOST || DEFAULT_HOST;
+export const port = PORT || 3000;
+export const devHost = DEV_HOST || DEFAULT_HOST;
+export const devPort = DEV_PORT || 3001;
+export const apiHost = API_HOST || DEFAULT_HOST;
+export const apiPort = API_PORT || 3002;
+export const v1Protocol = V1Protocol;
+export const v1Host = V1Host;
+export const v1Instance = V1Instance;
