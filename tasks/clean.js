1a49262a9df15ff6054b73431fdcee4a2f0557e6
walker smith <walker.smith@versionone.com>
2016-09-06 16:44:52 -0400

    Use gulp-rm when cleaning.

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
+var rm = require( 'gulp-rm' );
 
-gulp.task('clean', shell.task([
-    'rm -rf dist/',
-    'rm -rf public/'
-]));
+gulp.task('clean', function() {
+    gulp.src( 'public/**/*', { read: false })
+        .pipe(rm());
+    gulp.src( 'dist/**/*', { read: false })
+        .pipe(rm());
+});
