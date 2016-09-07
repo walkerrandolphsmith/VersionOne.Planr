57e9a119eaf33ea9e90a749985d18a6829cd6c6d
walker smith <walker.smith@versionone.com>
2016-09-07 09:54:58 -0400

    Start does not build or create the env file.

--- tasks/start.js
+++ tasks/start.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var env = require('gulp-env');
 
-gulp.task('start', ['dotenv', 'build'], function() {
+gulp.task('start', [], function() {
     const envVariables = env({
         file: '.env.json'
     });
