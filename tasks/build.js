f6e7075c8c71c7113877ad9e01fe09e8d9424ef4
Walker Randolph Smith <walkerrandolphsmith@users.noreply.github.com>
2016-09-13 10:32:06 -0400

    Webpacking the server breaks dotenv & env.js

--- tasks/build.js
+++ tasks/build.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var env = require('gulp-env');
 
-gulp.task('build:client', ['dotenv'], function() {
+/*gulp.task('build:client', ['dotenv'], function() {
     const envVariables = env({
         file: '.env.json',
         vars: {
@@ -28,7 +28,16 @@ gulp.task('build:server', ['dotenv'], function() {
         .pipe(shell([
             'webpack -p --config webpack.production.server.config.js'
         ]));
-});
+});*/
+
+gulp.task('build:client', shell.task([
+    'webpack -p --config webpack.production.config.js'
+]));
+
+gulp.task('build:server', shell.task([
+    'babel src/server -d dist/server',
+    'babel src/shared -d dist/shared'
+]));
 
 gulp.task('build', ['clean', 'build:client', 'build:server'], function() {
 
