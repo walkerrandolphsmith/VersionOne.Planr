5a3921dc70ebd876467ee2a6e30f4cee19d1fc8a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-15 18:38:24 -0400

    Clean task no longer needs to remove the public dir.

--- tasks/clean.js
+++ tasks/clean.js
@@ -2,8 +2,6 @@ var gulp = require('gulp');
 var rm = require( 'gulp-rm' );
 
 gulp.task('clean', function() {
-    gulp.src( 'public/**/*', { read: false })
-        .pipe(rm());
     gulp.src( 'dist/**/*', { read: false })
         .pipe(rm());
 });
